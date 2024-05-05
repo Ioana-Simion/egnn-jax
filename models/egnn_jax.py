4817491367ab98ab244cc21d6537389e800ad57d
Jonas <jonas.schaefer98@gmail.com>
2024-05-05 15:16:11 +0200

    fix callable bug

--- models/egnn_jax.py
+++ models/egnn_jax.py
@@ -14,33 +14,37 @@ class E_GCL(nn.Module):
     """
     E(n) Equivariant Message Passing Layer
     """
-    hidden_dim: int
+    hidden_nf: int
     edges_in_d: int
     act_fn: callable
     residual: bool
 
     def edge_model(self, edge_index, h, coord, edge_attr):
+
         row, col = edge_index
         source, target = h[row], h[col]
-        radial, coord_diff = self.coord2radial(edge_index, coord)
+        radial = self.coord2radial(edge_index, coord)
 
         edge_mlp = nn.Sequential([
-            nn.Dense(self.hidden_dim),
+            nn.Dense(self.hidden_nf),
             self.act_fn,
-            nn.Dense(self.hidden_dim),
+            nn.Dense(self.hidden_nf),
             self.act_fn
         ])
+
         out = jnp.concatenate([source, target, radial, edge_attr], axis=1)
+
         return edge_mlp(out)
+
 
     def node_model(self, edge_index, edge_attr, x):
         row, col = edge_index
-        agg = unsorted_segment_sum(edge_attr, row, num_segments=x.size(0))
+        agg = unsorted_segment_sum(edge_attr, row, num_segments=x.shape[0])
 
         node_mlp = nn.Sequential([
-            nn.Dense(self.hidden_dim),
+            nn.Dense(self.hidden_nf),
             self.act_fn,
-            nn.Dense(self.hidden_dim)
+            nn.Dense(self.hidden_nf)
         ])
 
         agg = jnp.concatenate([x, agg], axis=1)
@@ -52,11 +56,13 @@ class E_GCL(nn.Module):
     def coord_model(self, edge_index, edge_feat, coord):
         row, col = edge_index
         coord_mlp = nn.Sequential([
-            nn.Dense(self.hidden_dim),
+            nn.Dense(self.hidden_nf),
             self.act_fn,
-            nn.Dense(self.hidden_nf, 1, kernel_init=xavier_init(gain=0.001))
+            nn.Dense(1, kernel_init=xavier_init(gain=0.001))
         ])
-        trans = (x[row] - x[col]) * coord_mlp(edge_feat)
+
+        coord_out = coord_mlp(edge_feat)
+        trans = (coord[row] - coord[col]) * coord_out
 
         agg = unsorted_segment_mean(trans, row, num_segments=coord.size(0))
 
@@ -64,10 +70,10 @@ class E_GCL(nn.Module):
         return coord
 
     def coord2radial(self, edge_index, coord):
-        row, col = edge_index
-        coord_diff = coord[row] - coord[col]
-        radial = jnp.sum(coord_diff**2, axis=1, keepdims=True)
-        return radial, coord_diff
+        senders, receivers = edge_index
+        coord_i, coord_j = coord[senders], coord[receivers]
+        distance = jnp.sum((coord_i - coord_j) ** 2, axis=1, keepdims=True)
+        return distance
 
 
     @nn.compact
@@ -79,31 +85,25 @@ class E_GCL(nn.Module):
 
 
 class EGNN(nn.Module):
-    in_node_nf: int
     hidden_nf: int
     out_node_nf: int
     in_edge_nf: int = 0
-    act_fn = nn.silu  # default activation function
+    act_fn : callable = nn.silu  # default activation function
     n_layers: int = 4
     residual: bool = True
-
 
     @nn.compact
     def __call__(self, h, x, edges, edge_attr):
         h = nn.Dense(self.hidden_nf)(h)
         for i in range(self.n_layers):
-            layer = E_GCL(self.hidden_nf, edges_in_d=self.in_edge_nf, act_fn=self.act_fn, residual=self.residual) #name=f"gcl_{i}"
-            h, x, _ = layer(h, edges, x, edge_attr=edge_attr)
+            h, x, _ = E_GCL(self.hidden_nf, edges_in_d=self.in_edge_nf, act_fn=self.act_fn,
+                            residual=self.residual)(h, edges, x, edge_attr=edge_attr) #name=f"gcl_{i}"
         h = nn.Dense(self.out_node_nf)(h)
         return h, x
 
 
 def unsorted_segment_sum(data, segment_ids, num_segments):
-    result_shape = (num_segments, data.size(1))
-    result = data.new_full(result_shape, 0)  # Init empty result tensor.
-    segment_ids = segment_ids.unsqueeze(-1).expand(-1, data.size(1))
-    result.scatter_add_(0, segment_ids, data)
-    return result
+    return jax.ops.segment_sum(data, segment_ids, num_segments)
 
 
 def unsorted_segment_mean(data, segment_ids, num_segments):
@@ -127,10 +127,11 @@ def get_edges(n_nodes):
     edges = [rows, cols]
     return edges
 
+
 def get_edges_batch(n_nodes, batch_size):
     edges = get_edges(n_nodes)
-    edge_attr = jnp.ones((len(edges[0]) * batch_size, 1))
-    edges = [jnp.array(edges[0], dtype=jnp.int32), jnp.array(edges[1], dtype=jnp.int32)]
+    edge_attr = jnp.ones(len(edges[0]) * batch_size, dtype=jnp.float32).reshape(-1, 1)
+    edges = [jnp.array(edges[0]).astype(jnp.int32), jnp.array(edges[1]).astype(jnp.int32)]
     if batch_size == 1:
         return edges, edge_attr
     elif batch_size > 1:
@@ -157,7 +158,7 @@ if __name__ == "__main__":
     rng = jax.random.PRNGKey(42)
 
     # Initialize EGNN
-    egnn = EGNN(in_node_nf=n_feat, hidden_nf=32, out_node_nf=1, in_edge_nf=1)
+    egnn = EGNN(hidden_nf=32, out_node_nf=1, in_edge_nf=1)
 
     params = egnn.init(rng, h, x, edges, edge_attr)['params']
 
